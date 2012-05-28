65f61f846d84a6c55cae8d9a20fb3f643246b0ef
Steve Derico <Steve@SteveDerico.com>
2012-05-28 12:21:12 -0700

    Improved SDFooterButtonViewStyleGray

--- SDFooterButtonView.m
+++ SDFooterButtonView.m
@@ -17,10 +17,12 @@
         self.button= [UIButton buttonWithType:UIButtonTypeRoundedRect];     
         [self.button setTitle:@"Sample Text" forState:UIControlStateNormal];
         self.button.titleLabel.font = [UIFont fontWithName:@"Helvetica-Bold" size:23];
-        
+    
+
         [self.button setTitleShadowColor:[UIColor blackColor] forState:UIControlStateNormal];
-        [[self.button titleLabel] setShadowOffset:CGSizeMake(0, -.5f)];
-        
+        [[self.button titleLabel] setShadowOffset:CGSizeMake(0, -.50f)];
+
+                
         //Default Button
         [self.button setTitleColor:[UIColor darkGrayColor] forState:UIControlStateNormal];
         [self.button setBackgroundImage:[UIImage imageNamed:@"Green_background.png"] forState:UIControlStateNormal];
@@ -37,7 +39,12 @@
             [self.button setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
             [self.button setBackgroundImage:[UIImage imageNamed:@"Green_background.png"] forState:UIControlStateNormal];
         }else if (style == SDFooterButtonStyleGray){
-            [self.button setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+            [self.button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+            [self.button setTitleShadowColor:[UIColor whiteColor] forState:UIControlStateNormal];
+            [[self.button titleLabel] setShadowOffset:CGSizeMake(0, 1.0f)];
+            [self.button setTitleShadowColor:[UIColor blackColor] forState:UIControlStateHighlighted];
+             [self.button setTitleColor:[UIColor whiteColor] forState:UIControlStateHighlighted];
+
             [self.button setBackgroundImage:[UIImage imageNamed:@"gray_button.png"] forState:UIControlStateNormal];
 
         }else if (style == SDFooterButtonStyleRed){
--- SDSwitchCell.h
+++ SDSwitchCell.h
@@ -3,7 +3,7 @@
 //  
 //
 //  Created by Steve Derico on 10/18/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Copyright 2011 Bixby Apps. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
